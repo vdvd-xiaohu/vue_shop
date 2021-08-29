e4a8749212b3197abf06f0ce727b84072fdee8b9
vdvd <2641185063@qq.com>
2021-08-29 11:43:11 +0800

    完成了登录功能

--- src/main.js
+++ src/main.js
@@ -2,10 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
-
+// 导入全局样式表
+import './assets/css/global.css'
+import axios from 'axios'
+Vue.prototype.$http = axios
+axios.defaults.baseURL = 'https://lianghj.top:8888/api/private/v1/'
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  render: h => h(App)
+    router,
+    render: h => h(App)
 }).$mount('#app')
